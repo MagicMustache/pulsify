1dd945d387b9e935141eec1f088b39e725f65ac9
MagicMustache <milo.gianinazzi@gmail.com>
2021-04-26 18:51:05 +0200

    fetch spotify user's playlists

--- src/App.js
+++ src/App.js
@@ -1,30 +1,83 @@
 import './App.css';
 import Camera from "./components/Camera";
-import { SpotifyApiContext } from 'react-spotify-api'
-import { SpotifyAuth, Scopes } from 'react-spotify-auth'
+import React, {useEffect, useState} from 'react';
+import {Scopes, SpotifyAuth} from 'react-spotify-auth'
+import Axios from "axios"
 import Cookies from 'js-cookie'
+import SpotifyWebApi from "spotify-web-api-js";
+import Playlist from "./components/Playlist";
 
 function App() {
     const token = Cookies.get('spotifyAuthToken')
-        return (
-            <div className={"container-fluid justify-content-center align-content-center align-items-center"} style={{}}>
-                <h1 className={"text-center"}>Pulsify</h1>
-                {token ? (
-                    <SpotifyApiContext.Provider value={token}>
-                        <Camera/>
-                        <h3>You are connected to spotify</h3>
-                    </SpotifyApiContext.Provider>
-                ):(
-                    <SpotifyAuth
-                        redirectUri='http://localhost:3000'
-                        clientID='dd7a0938872c4219b6b83bbe40cb5404'
-                        scopes={[Scopes.userReadPrivate, 'user-read-email']} // either style will work
-                    />
-                )
-            }
-            </div>
+    const [userId, setUserId] = useState("")
+    const [userPlaylists, setPlaylists] = useState([])
+    const [startCam, setStartCam] = useState(false)
+    const spotifyApi = new SpotifyWebApi();
 
+    useEffect(() => {
+        getUserID()
+    }, [])
+
+    if (token) {
+        spotifyApi.setAccessToken(token)
+
+        return (
+            <div className={"container"} style={{}}>
+                <h1 className={"text-center"}>Pulsify</h1>
+                <div className={"d-flex flex-column"}>
+                    {startCam ? <Camera/> : null}
+                    <div className={"d-flex d-flex-column justify-content-center"}>
+                        <h3>You are connected as "{userId}" on Spotify{"\n"}</h3>
+                    </div>
+                    <div className={"d-flex d-flex-row justify-content-center"}>
+                        <button className={"btn btn-info"} onClick={() => getUserPlaylists()}>
+                            Get all playlists
+                        </button>
+                        <button className={"btn btn-danger"} onClick={() => Cookies.remove("spotifyAuthToken")}
+                                style={{marginLeft: "2em"}}>
+                            Clear Cookies
+                        </button>
+                    </div>
+                    <div className={"d-flex flex-column"}>
+                        {userPlaylists.length > 0 ? (generatePlaylistsComponents(userPlaylists)) : null}
+                    </div>
+                </div>
+            </div>
         );
+    } else {
+        return (
+            <SpotifyAuth
+                redirectUri='http://localhost:3000'
+                clientID='dd7a0938872c4219b6b83bbe40cb5404'
+                scopes={[Scopes.userReadPrivate, 'user-read-email', Scopes.playlistReadPrivate, Scopes.playlistReadCollaborative]}/>
+        )
+    }
+
+
+    function getUserID() {
+        Axios.get("https://api.spotify.com/v1/me", {
+            headers: {
+                "Authorization": "Bearer " + token,
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        }).then(r => {
+            console.log("user id: " + r.data.id)
+            setUserId(r.data.id)
+        })
+    }
+
+    function getUserPlaylists() {
+        spotifyApi.getUserPlaylists(userId).then((res) => {
+            setPlaylists(res.items)
+        })
+    }
+}
+
+function generatePlaylistsComponents(playlists) {
+    return playlists.map((playlist =>
+            <Playlist key={playlist.id} playlist={playlist}/>
+    ))
 }
 
 
---
+++ src/components/Playlist.js
@@ -0,0 +1,18 @@
+function Playlist(props) {
+    if (props.playlist.name) {
+        return (
+            <div className={"card"} style={{margin:"2em", width:"50%"}}>
+                <img className={"card-img-top"} src={props.playlist.images[0].url} style={{width: "150px", height: "150px", margin:"1em"}}/>
+                <div className={"card-body"}>
+                    <h5 className={"card-title"} style={{width:"50%"}}>{props.playlist.name}</h5>
+                    <button className={"btn btn-primary"} style={{}}>Choose this playlist</button>
+                </div>
+            </div>
+        )
+    } else {
+        return null
+    }
+
+}
+
+export default Playlist
