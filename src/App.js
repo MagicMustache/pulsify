b16b8e38ea5dc6800ce092d0ea57ef9e8e47a789
Valentin Tamone <tamtam.valentin@gmail.com>
2021-05-11 12:36:33 +0200

    Added EmpaticaE4 client

--- src/App.js
+++ src/App.js
@@ -59,6 +59,21 @@ function App() {
             )
         }
 
+        //Empatica
+        var EmpaticaE4 = require('empatica-e4-client');
+        var dev1 = new EmpaticaE4();
+
+        var portNumber  = 28000;
+        var ipAddress   = '127.0.0.1';
+        var deviceID    = '543a64';        //Empatica E4 device ID //543a64 ou 484c5c
+        dev1.connect(portNumber ,ipAddress, deviceID, function(data){
+            var sensorData = EmpaticaE4.getString(data);
+            console.log(sensorData);
+        });
+        setTimeout(function() {
+            dev1.subscribe(EmpaticaE4.E4_BVP);
+        }, 1000);
+
         return (
             <div className={"container"} style={{}}>
                 <h1 className={"text-center"}>Pulsify</h1>
